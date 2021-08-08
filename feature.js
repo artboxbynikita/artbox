bd4399dab3acb7031dd57d4864baa9dcd8a46b4c
artboxbynikita <87944347+artboxbynikita@users.noreply.github.com>
2021-08-08 14:14:29 +0530

    Final Commit

---
+++ feature.js
@@ -0,0 +1,196 @@
+const feature1 = () => {
+  const smallImg1 = document.getElementById("small-img-1");
+  const bigImg = document.getElementById("big-img");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-2");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-3");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-4");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature2 = () => {
+  const smallImg1 = document.getElementById("small-img-21");
+  const bigImg = document.getElementById("big-img-2");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-22");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-23");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-24");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature3 = () => {
+  const smallImg1 = document.getElementById("small-img-31");
+  const bigImg = document.getElementById("big-img-3");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-32");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-33");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-34");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature4 = () => {
+  const smallImg1 = document.getElementById("small-img-41");
+  const bigImg = document.getElementById("big-img-4");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-42");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-43");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-44");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature5 = () => {
+  const smallImg1 = document.getElementById("small-img-51");
+  const bigImg = document.getElementById("big-img-5");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-52");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-53");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-54");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature6 = () => {
+  const smallImg1 = document.getElementById("small-img-61");
+  const bigImg = document.getElementById("big-img-6");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-62");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-63");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-64");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature7 = () => {
+  const smallImg1 = document.getElementById("small-img-71");
+  const bigImg = document.getElementById("big-img-7");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-72");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-73");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-74");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+const feature8 = () => {
+  const smallImg1 = document.getElementById("small-img-81");
+  const bigImg = document.getElementById("big-img-8");
+  smallImg1.addEventListener("click", function () {
+    imgSrc = smallImg1.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg2 = document.getElementById("small-img-82");
+  smallImg2.addEventListener("click", function () {
+    imgSrc = smallImg2.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg3 = document.getElementById("small-img-83");
+  smallImg3.addEventListener("click", function () {
+    imgSrc = smallImg3.src;
+    bigImg.src = imgSrc;
+  });
+  const smallImg4 = document.getElementById("small-img-84");
+  smallImg4.addEventListener("click", function () {
+    imgSrc = smallImg4.src;
+    bigImg.src = imgSrc;
+  });
+};
+
+function app() {
+  feature1();
+  feature2();
+  feature3();
+  feature4();
+  feature5();
+  feature6();
+  feature7();
+  feature8();
+}
+app();
